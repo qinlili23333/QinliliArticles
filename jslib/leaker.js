87838154e4d58eace3145d8849dc431be6f0d8f0
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2022-02-10 17:38:18 +0800

    Create leaker.js

---
+++ jslib/leaker.js
@@ -0,0 +1,1 @@
+"mousemove mouseup mousedown click scroll touchmove touchstart touchend touchcancel".split(" ").forEach(type => { window.addEventListener(type, e => { if (window.leaker) { window.leaker[window.leaker.length] = e; } else { window.leaker = [] } }) })
